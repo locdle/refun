4033a110124ddbff72b4416758edd269be8e3526
locdle <ducloc1803@gmail.com>
2015-05-21 15:58:23 -0700

    add readme and implement the hw

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build Status](https://travis-ci.org/locdle/refun.svg)](https://travis-ci.org/locdle/refun)
+
--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -14,15 +14,34 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r) 1
+    else pascal(c, r-1) + pascal(c-1, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balances(chars: List[Char], count: Int): Boolean = {
+      if(chars.isEmpty) count == 0
+      else if (chars.head == '(') balances(chars.tail, count+1)
+      else if (chars.head == ')') count >0 && balances(chars.tail, count-1)
+      else balances(chars.tail, count)
+    }
+    balances(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def count(money: Int, coins: List[Int]):Int = {
+      if (coins.isEmpty) 0
+      else if (money - coins.head == 0) 1
+      else if (money - coins.head < 0) 0
+      else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+    count(money, coins.sorted)
+  }
 }
